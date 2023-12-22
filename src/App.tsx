b48fe99224ac7f25190e4f3206f633f1e2b1e6dd
Popov Evgenii <ev.popov@elma-bs.ru>
2023-12-22 20:12:52 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className="playingCards faceImages">
+          <span className="card rank-a diams">
+            <span className="rank">A</span>
+            <span className="suit">♦</span>
+          </span>
+          <span className="card rank-q diams">
+            <span className="rank">Q</span>
+            <span className="suit">♦</span>
+          </span>
+          <span className="card rank-9 diams">
+            <span className="rank">9</span>
+            <span className="suit">♦</span>
+          </span>
+          <span className="card rank-2 diams">
+            <span className="rank">2</span>
+            <span className="suit">♦</span>
+          </span>
+          <span className="card rank-k diams">
+            <span className="rank">K</span>
+            <span className="suit">♦</span>
+          </span>
+      </div>
+  )
+}
+
+export default App;
